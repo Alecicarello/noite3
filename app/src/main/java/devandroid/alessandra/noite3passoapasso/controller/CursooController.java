e75582776b07a269a55e52d4bfe6b92261c5de68
Alecicarello <144715168+Alecicarello@users.noreply.github.com>
2023-09-28 15:57:36 -0300

    correção 28.09

--- app/src/main/java/devandroid/alessandra/noite3passoapasso/controller/CursooController.java
+++
@@ -1,4 +0,0 @@
-package devandroid.alessandra.noite3passoapasso.controller;
-
-public class CursooController {
-}
--- app/src/main/java/devandroid/alessandra/noite3passoapasso/view/MainActivity.java
+++ app/src/main/java/devandroid/alessandra/noite3passoapasso/view/MainActivity.java
@@ -16,7 +16,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         pessoa = new Pessoa();
+        //Atribuir conteúdo, dados, valores para o Objeto
+        //Conforme o seu MODELO, TEMPLATE
+        pessoa.setPrimeiroNome("Alessandra");
+        pessoa.setSobreNome("Cicarello");
+        pessoa.setCursoDesejado("Amdroid");
+        pessoa.setTelefoneContato("31991826795");
+
     }
 
 }
+
+
+
